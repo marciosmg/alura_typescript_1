de4d5cc9023e9499f82c716efc60d5e007d745ef
marciosmg <marciosmg@gmail.com>
2022-05-22 21:47:25 -0300

    Fim da aula 05 do curso TypeScript 1 da Alura

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -2,12 +2,14 @@ import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
+    // Array<Negociação> pode ser escrito também como -> Negociacao[]
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
     lista(): ReadonlyArray<Negociacao> {
+        // ReadOnlyArray<Negociação> pode ser escrito também como -> readonly Negociacao[]
         return this.negociacoes;
     }
 }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,10 +2,12 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
+    // Array<Negociação> pode ser escrito também como -> Negociacao[]
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
     lista() {
+        // ReadOnlyArray<Negociação> pode ser escrito também como -> readonly Negociacao[]
         return this.negociacoes;
     }
 }
